c5da0f0c64207f8d8bb321d7958cd4d45f8242b1
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-05-02 12:27:49 +0600

    first commit

--- index.py
+++ index.py
@@ -6,8 +6,6 @@ def generate_password(length=12):
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
-# Example usage: generate a password with default length of 12 characters
 print("Generated password:", generate_password())
 
-# Example usage: generate a password with custom length of 16 characters
 print("Generated password (length 16):", generate_password(16))
